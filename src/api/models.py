e0dedf7ea644b6de42a67cdcbf0029ba4c923c94
C137Adrian <adriancanoespinosa@gmail.com>
2025-06-16 10:03:56 +0000

    event model update

--- src/api/models.py
+++ src/api/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, DateTime
+from sqlalchemy import String, Boolean
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
@@ -18,18 +18,3 @@ class User(db.Model):
             # do not serialize the password, its a security breach
         }
     
-class Event(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str] = mapped_column(String(200), nullable=False)
-    description: Mapped[str] = mapped_column(Text, nullable=True)
-    date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
-    created_user_id: Mapped[int] = mapped_column(ForeignKey("association.id"))
-    volunteers_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
-
-   def serializa(self):
-    return {
-        "id": self.id,
-            "title": self.title,
-            "description": self.description,
-            "date": self.date.isoformat()
-    }
---
+++ src/api/models/events.py
@@ -0,0 +1,34 @@
+from sqlalchemy import String, ForeignKey, Text, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from datetime import datetime
+from . import db
+
+
+
+class Event(db.Model):
+    __tablename__ = "events"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(200), nullable=False)
+    description: Mapped[str] = mapped_column(Text, nullable=True)
+    image_url: Mapped[str] = mapped_column(String(500), nullable=True)
+    date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+
+    association_id: Mapped[int] = mapped_column(ForeignKey("associations.id"), nullable=False)
+    volunteer_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=True)
+
+    association = relationship ("Association", backref="events")  
+    volunteer = relationship ("User", backref="volunteered_events")
+
+    def serializa(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "image_url": self.image_url,
+            "date": self.date.isoformat(),
+            "association_id": self.association_id,
+            "association_name": self.association.name if self.association else None,
+            "volunteer_id": self.volunteer_id,
+            "volunteer_name": f"{self.volunteer.name} {self.volunteer.lastname}" if self.volunteer else None
+        }
