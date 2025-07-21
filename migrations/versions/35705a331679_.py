1741edebdef1f389283d7b14ff90b8eb9c66b7fa
sandrodevx <sandrodevx@gmail.com>
2025-07-21 21:43:32 +0200

    Fix migration conflicts and update to clean migration from develop

--- migrations/versions/35705a331679_.py
+++ migrations/versions/35705a331679_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: a55898084bb8
+Revision ID: 35705a331679
 Revises: 
-Create Date: 2025-07-21 19:34:38.854555
+Create Date: 2025-07-18 19:17:32.891792
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'a55898084bb8'
+revision = '35705a331679'
 down_revision = None
 branch_labels = None
 depends_on = None
